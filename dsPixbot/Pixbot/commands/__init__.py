5d93f09558fecbc04adcf3d00e1a8fe381ba0ec6
Sakamoto0110 <rafael.sakamoto1@hotmail.com>
2022-03-15 15:24:19 -0300

    .

---
+++ dsPixbot/Pixbot/commands/__init__.py
@@ -0,0 +1,5 @@
+from pixbot.commands.core import *
+from pixbot.commands.database import *
+from pixbot.commands.games import *
+from pixbot.commands.math import *
+from pixbot.commands.socialInteractions import *
